3324498b8f5ca2e58348b60e45ff6f6287e7b6e9
Aaron Fraga <aronfraga@gmail.com>
2022-11-08 12:06:59 -0300

    routes created

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './components/Home/Home';
 
 function App() {
   return (
-    <div>
-      APP GYM
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/home' element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
