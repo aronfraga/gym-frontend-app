e400066cd24507bfbfa100ce72dbb044a93d2497
Aaron Fraga <aronfraga@gmail.com>
2022-11-10 09:35:47 -0300

    route fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,26 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./components/Home/Home";
+import NoMatch from "./components/NoMatch/NoMatch";
+import Routines from "./components/Routines/Routines";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path='*' element={<NoMatch />} />
         <Route path='/' element={<Home />} />
-        <Route path='/home' element={<Home />} />
-        <Route path='/home/rutinas' element={<Home />} />
+        <Route path='/tienda' element={<Home />} />
+        <Route path='/rutinas' element={<Routines />} />
+        <Route path='/instalaciones' element={<Home />} />
+        <Route path='/seguimiento' element={<Home />} />
+        <Route path='/clases' element={<Home />} />
+        <Route path='/feedback' element={<Home />} />
+        <Route path='/planes' element={<Planes />} />
       </Routes>
     </BrowserRouter>
   );
 }
 
 export default App;
+
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,16 +1,18 @@
 import React, { useEffect } from "react";
 import { useAuth0 } from '@auth0/auth0-react';
-import Login from "../Login/Login";
+import { Link } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { setToken } from "../../redux/actions/defaultAction";
 import NavBar from "../NavBar/NavBar";
 import Footer from "../Footer/Footer";
+import Loading from "../Loading/Loading";
 import Carrusel from "../Carousel/Carrusel";
+import Login from "../Login/Login";
 
 const Home = () => {
 
   const dispatch = useDispatch();
-  const { user, isAuthenticated, logout } = useAuth0();
+  const { user, isAuthenticated, isLoading } = useAuth0();
 
   useEffect(() => {
     if (isAuthenticated) dispatch(setToken(user.sub));
@@ -31,13 +33,16 @@ const Home = () => {
   }
 
   const renderSpinner = () => {
-    return
+    return <Loading />
   }
 
   return (
     <div>
-      {isAuthenticated ? renderisAuthenticated() : renderisNotAuthenticated()}
-
+      {isLoading ? renderSpinner() : isAuthenticated ? renderisAuthenticated() : renderisNotAuthenticated()}
+      <Link to='home/routines'>
+        <button>routines</button>
+      </Link>
+      
     </div>
   )
 }
