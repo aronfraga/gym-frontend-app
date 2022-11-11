fe9a028613e6b4f22d5012d44ff80d3b0a7d8ef1
Aaron Fraga <aronfraga@gmail.com>
2022-11-11 11:19:17 -0300

    btn for filter created

---
+++ src/components/BtnFilter/BtnFilter.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import Button from '@mui/material/Button';
+import Icon from '@mui/material/Icon';
+import ButtonGroup from '@mui/material/ButtonGroup';
+import style from './BtnFilter.module.css';
+
+const BtnFilter = () => {
+  return (
+    <div className={style.btnMaster}>
+      <Icon>star</Icon>
+      <ButtonGroup variant="contained" aria-label="outlined primary button group">
+        <Button>Todas las rutinas</Button>
+        <Button>Mis Rutinas</Button>
+        <Button>Las que no tengo</Button>
+      </ButtonGroup>
+      <Button variant="contained">Crear rutina</Button>
+    </div>
+  )
+}
+
+export default BtnFilter;
+
+
+
+
+
+
+
--- src/components/Routines/Routines.jsx
+++ src/components/Routines/Routines.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import NavBar from "../NavBar/NavBar";
 import Footer from "../Footer/Footer";
 import AllRoutines from "../AllRoutines/AllRoutines";
+import BtnRoutines from "../BtnRoutines/BtnRoutines";
 
 const Routines = () => {
   return (
     <div>
       <NavBar />
-        <AllRoutines />
+      <BtnRoutines />
+      <AllRoutines />
       <Footer />
     </div>
   )
