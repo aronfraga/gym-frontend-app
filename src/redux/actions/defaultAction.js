ac13edc4202f0e782c14b6fa9641ad3b1e4ec986
JAlex2014 <agomezf@pucp.edu.pe>
2022-11-13 23:11:40 -0500

    Componente Instalaciones

--- src/redux/actions/defaultAction.js
+++ src/redux/actions/defaultAction.js
@@ -1,4 +1,5 @@
-import { setCurrentPage, setAccessToken } from '../slices/defaultSlice';
+import { setCurrentPage, setAccessToken, getcloudImages} from '../slices/defaultSlice';
+import { Buffer } from "buffer";
 
 export const setPage = (data) => {
   return (dispatch) => {
@@ -11,3 +12,22 @@ export const setToken = (data) => {
     return dispatch(setAccessToken(data));
   }
 }
+
+export const fetchImages = () => {
+  return async function(dispatch){
+      try{ 
+          const results = await fetch('/api/v1_1/diapwgajv/resources/image', {
+          mode: "no-cors",
+          headers: {
+              "Authorization": `Basic ${Buffer.from(import.meta.env.CLOUDINARY_API_KEY + ':' + import.meta.env.CLOUDINARY_API_SECRET).toString('base64')}`
+          }}).then(res => res.json());
+          const cloudinary_array =  await results.resources;
+          const cloudinary_images = await cloudinary_array.filter(index =>  index.folder === "AppGym-facilities");
+          
+          return dispatch(getcloudImages(cloudinary_images));
+      
+      }catch(error){
+              console.log(error.message)
+      }
+  }
+};
--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -3,11 +3,13 @@ import { setupListeners } from '@reduxjs/toolkit/query';
 import { ApiQuery } from '../query/api';
 import currentPage from '../slices/defaultSlice';
 import accessToken from '../slices/defaultSlice';
+import facilitiesImages from '../slices/defaultSlice';
 
 export const store = configureStore({
   reducer: {
     currentPage: currentPage,
     accessToken: accessToken,
+    facilitiesImages,
     [ ApiQuery.reducerPath ]: ApiQuery.reducer,
   },
   middleware: (getDefaultMiddleware) =>
