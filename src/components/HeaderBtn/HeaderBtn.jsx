e42322ca104e2fe1a8d7c8420e48a5922b233520
ManuCasanueva <manucasanueva3@gmail.com>
2022-11-20 19:49:00 -0300

    add ShoppingCart component and solve bugs in navBar

--- src/components/HeaderBtn/HeaderBtn.jsx
+++ src/components/HeaderBtn/HeaderBtn.jsx
@@ -1,25 +1,16 @@
 import React from 'react';
-import {useSelector} from "react-redux";
-import { Link } from 'react-router-dom';
 import BtnFilter from '../BtnFilter/BtnFilter';
+import ShoppingCart from '../ShoppingCart/ShoppingCart';
 import style from './HeaderBtn.module.css';
-import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+
 
 const HeaderBtn = ({ title }) => {
-
-	const {cart_count} = useSelector(state => state.cart_count);
-
 	return (
 		<div className={style.mainContainer}>
 			<div className={style.infoContainer}>
 				<h1 className={style.title}>{title}</h1>
 				{/* <BtnFilter/> */}
-				<div className={style.ShoppingCart}>
-					<Link style={{color:"black"}}>
-                        <ShoppingCartIcon/>
-                        <span className={style.cartspan}>{cart_count}</span>
-					</Link>
-                </div>
+				<ShoppingCart />
 			</div>
 			<hr />
 		</div>
