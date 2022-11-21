3f47ea430b6c8ec07bfc20cb1694b0fa56fb9187
AguRey96 <agureynoso96@gmail.com>
2022-11-20 23:20:27 -0300

    cambios para relajar

--- src/components/Staff/Staff.jsx
+++ src/components/Staff/Staff.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect} from "react";
+import React, { useEffect } from "react";
 import { staticstaff } from "./Helpers.js";
 import { fetchGetAllStaff } from "../../redux/actions/defaultAction.js";
 import { useSelector, useDispatch } from "react-redux";
@@ -8,30 +8,33 @@ import NavBar from "../NavBar/NavBar.jsx";
 import Footer from "../Footer/Footer.jsx";
 
 const Staff = () => {
-    const dispatch = useDispatch();
-    const {staff} = useSelector(state => state.staff);
-    useEffect(() => {dispatch(fetchGetAllStaff())},[dispatch]);
-    return (
-        <>
-            <NavBar />
-            <div className={Style.mainwrapper}>
-                {/* {console.log(staff)} */}
-                <h1 className={Style.stafftittle}>Staff Técnico</h1>
-                <hr className={Style.divisionline}></hr>
-                <div className={Style.cardswrapper}>
-                    {staticstaff?.map(staff =>
-                        <CardStaff key={staff.hashPassword}
-                            name={staff.name}
-                            linkedin={staff.linkedin}
-                            rating={staff.rating}
-                            img={staff.picture}
-                            mail={staff.email}
-                        />)
-                    }
-                </div>
-            </div>
-            <Footer/>
-        </>
-    );
-}
+  const dispatch = useDispatch();
+  const { staff } = useSelector((state) => state.staff);
+  useEffect(() => {
+    dispatch(fetchGetAllStaff());
+  }, [dispatch]);
+  return (
+    <>
+      <NavBar />
+      <div className={Style.mainwrapper}>
+        {/* {console.log(staff)} */}
+        <h1 className={Style.stafftittle}>Staff Técnico</h1>
+        <hr className={Style.divisionline}></hr>
+        <div className={Style.cardswrapper}>
+          {staticstaff?.map((staff) => (
+            <CardStaff
+              key={staff.hashPassword}
+              name={staff.name}
+              linkedin={staff.linkedin}
+              rating={staff.rating}
+              img={staff.picture}
+              mail={staff.email}
+            />
+          ))}
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
+};
 export default Staff;
