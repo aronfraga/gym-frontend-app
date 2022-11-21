aff68dddae93cc66c1ad071a7f1fd0348baef62e
Aaron Fraga <aronfraga@gmail.com>
2022-11-21 18:19:31 -0300

    404 fixed

--- src/main.jsx
+++ src/main.jsx
@@ -9,16 +9,16 @@ import FilterProvider from "./components/Routines/FiltersContex";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-{/*     <Auth0Provider
+    <Auth0Provider
       domain="dev-whzziz8z5264izvj.us.auth0.com"
       clientId="1Cumcs4UOcjpGYzsin8CpBNfxLgM9gnN"
       redirectUri={`${window.location.origin}/home`}
-    > */}
+    >
       <FilterProvider>
         <Provider store={store}>
           <App />
         </Provider>
       </FilterProvider>
-{/*     </Auth0Provider> */}
+    </Auth0Provider>
   </React.StrictMode>
 );
