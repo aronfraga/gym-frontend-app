2fa74a4e91b3ab2f57943cdbf1862e1a7ef61ab9
Aaron Fraga <aronfraga@gmail.com>
2022-11-20 21:33:57 -0300

    crazy update

--- src/redux/query/ApiEcommerce.js
+++ src/redux/query/ApiEcommerce.js
@@ -6,11 +6,11 @@ export const ApiEcommerce = createApi({
   baseQuery: fetchBaseQuery({
     // baseUrl: 'https://appgymbackend-production.up.railway.app'
     baseUrl: "http://localhost:3001",
-    /*prepareHeaders: (headers) => {
+    prepareHeaders: (headers) => {
       const token = getToken().token;
-      if (token) headers.set("authorization", `Bearer ${token}`);
+        if (token) headers.set("authorization", `Bearer ${token}`);
       return headers;
-    },*/
+    },
   }),
   endpoints: (builder) => ({
     getAllProducts: builder.query({
@@ -19,14 +19,21 @@ export const ApiEcommerce = createApi({
         method: "get",
       }),
     }),
-    getFilteredProducts: builder.query({
+    getFilteredByPrice: builder.mutation({ // agus esta es la que arme yo
       query: (dataMin, dataMax) => ({
         url: "/products/filter",
         method: "post",
         body: { filters: { min: dataMin, max: dataMax }},
       }),
     }),
+    getFilteredByCategory: builder.query({
+      query: (data) => ({
+        url: "/products/filter",
+        method: "post",
+        body: data,
+      }),
+    }),
   }),
 });
 
-export const { useGetAllProductsQuery, useGetFilteredProductsQuery } = ApiEcommerce;
+export const { useGetAllProductsQuery, useGetFilteredByPriceMutation, useGetFilteredByCategoryQuery } = ApiEcommerce;
