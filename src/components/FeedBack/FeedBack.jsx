5266f040824bbc5c2bee82fa215a38b57500aa36
JoseManriqueBaena <josed3h@gmail.com>
2022-11-21 21:50:44 -0500

    Fixed staff and feedback

--- src/components/FeedBack/FeedBack.jsx
+++ src/components/FeedBack/FeedBack.jsx
@@ -1,9 +1,9 @@
-import React, { useEffect } from "react";
+import React, { useEffect } from 'react';
 import FormControl from '@mui/material/FormControl';
-import { useState } from "react";
-import style from "../FeedBack/FeedBack.module.css";
+import { useState } from 'react';
+import style from '../FeedBack/FeedBack.module.css';
 import Typography from '@mui/material/Typography';
-import NavBar from "../NavBar/NavBar.jsx";
+import NavBar from '../NavBar/NavBar.jsx';
 import { useTheme } from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import OutlinedInput from '@mui/material/OutlinedInput';
@@ -13,55 +13,48 @@ import Select from '@mui/material/Select';
 import Chip from '@mui/material/Chip';
 import Rating from '@mui/material/Rating';
 import StarIcon from '@mui/icons-material/Star';
-import { FormGroup, TextField } from "@mui/material"
+import { FormGroup, TextField } from '@mui/material';
 import Button from '@mui/material/Button';
 import { useNavigate } from 'react-router-dom';
-import { useAddFeedbackMutation, useGetAllStaffQuery } from "../../redux/query/api";
+import {
+	useAddFeedbackMutation,
+	useGetAllStaffQuery,
+} from '../../redux/query/api';
+import logo from '../../Images/Logo.png';
 // import { getAllStaff } from "../../redux/actions/defaultAction"
 // import { useDispatch, useSelector } from "react-redux";
-
-
-
-
 
 const ITEM_HEIGHT = 48;
 const ITEM_PADDING_TOP = 8;
 const MenuProps = {
-    PaperProps: {
-        style: {
-            maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
-            width: 250,
-        },
-    },
+	PaperProps: {
+		style: {
+			maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+			width: 250,
+		},
+	},
 };
 
 const labels = {
-    0.5: '',
-    1: '',
-    1.5: '',
-    2: '',
-    2.5: '',
-    3: '',
-    3.5: '',
-    4: '',
-    4.5: '',
-    5: '',
+	0.5: '',
+	1: '',
+	1.5: '',
+	2: '',
+	2.5: '',
+	3: '',
+	3.5: '',
+	4: '',
+	4.5: '',
+	5: '',
 };
 
-
-const dataGym = [
-    'Intalaciones',
-    'Maquinas',
-    'Servicios',
-    'Staff',
-    'Otros...'
-];
+const dataGym = ['Intalaciones', 'Maquinas', 'Servicios', 'Staff', 'Otros...'];
 
 // const staff = [
 //     'Martin Galara',
 //     'Gaston Schmitz',
 //     'Aaron Fraga',
-//     'Agustin Reynoso', 
+//     'Agustin Reynoso',
 //     'Jose Manrique',
 //     'Manuel Casanueva',
 //     'Pablo Lospennato',
@@ -69,227 +62,238 @@ const dataGym = [
 // ]
 
 function getStyles(name, infoGym, theme) {
-    return {
-        fontWeight:
-            infoGym.indexOf(name) === -1
-                ? theme.typography.fontWeightRegular
-                : theme.typography.fontWeightMedium,
-    };
+	return {
+		fontWeight:
+			infoGym.indexOf(name) === -1
+				? theme.typography.fontWeightRegular
+				: theme.typography.fontWeightMedium,
+	};
 }
-
 
 const FeedBack = () => {
+	// const dispatch = useDispatch()
+	// const { staff } = useSelector((state) => state.staff);
+	const { data: staff, isLoading } = useGetAllStaffQuery();
 
-    // const dispatch = useDispatch()
-    // const { staff } = useSelector((state) => state.staff);
-    const { data: staff, isLoading } = useGetAllStaffQuery()
+	const [addFeedback, { data }] = useAddFeedbackMutation();
+	const navigate = useNavigate();
+	const theme = useTheme();
+	const [input, setInput] = useState({
+		title: [],
+		staff: '',
+		description: '',
+		score: 0,
+		staffId: 0,
+	});
 
-    const [addFeedback, { data }] = useAddFeedbackMutation()
-    const navigate = useNavigate()
-    const theme = useTheme();
-    const [input, setInput] = useState({
-        title: [],
-        staff: "",
-        description: "",
-        score: 0,
-        staffId: 0,
-    });
+	const handleChange = (event) => {
+		if (event.target.name === 'staff') {
+			setInput({
+				...input,
+				[event.target.name]: event.target.value.name,
+				staffId: event.target.value.id,
+			});
+		} else if (event.target.name === 'score') {
+			const number = Number(event.target.value);
+			setInput({
+				...input,
+				[event.target.name]: number,
+			});
+		} else {
+			setInput({
+				...input,
+				[event.target.name]: event.target.value,
+			});
+		}
+	};
 
+	const HandleSubmit = async (e) => {
+		e.preventDefault();
+		await addFeedback({
+			title: input.title,
+			staff: input.staff,
+			description: input.description,
+			score: input.score,
+			staffId: input.staffId,
+		}).unwrap();
+		setInput({
+			title: [],
+			staff: '',
+			description: '',
+			score: 0,
+			staffId: 0,
+		});
+		alert('Gracias por dejarnos tu Feedback');
+		navigate('/home');
+	};
 
-    const handleChange = (event) => {
-        if (event.target.name === "staff") {
-            setInput({
-                ...input,
-                [event.target.name]: event.target.value.name,
-                staffId: event.target.value.id,
-            })
+	function getLabelText(value) {
+		return `${value} Star${value !== 1 ? 's' : ''}, ${labels[value]}`;
+	}
 
-        } else if (event.target.name === "score") {
-            const number = Number(event.target.value)
-            setInput({
-                ...input,
-                [event.target.name]: number,
-            })
-        } else {
-            setInput({
-                ...input,
-                [event.target.name]: event.target.value,
-            })
-        }
+	return (
+		<>
+			<NavBar />
+			<div className={style.mainContainer}>
+				<div className={style.mainContainerForm}>
+					<div className={style.logoGymContainer}>
+						<img className={style.logoGym} src={logo} />
+					</div>
+					<form onSubmit={HandleSubmit}>
+						<FormControl sx={{ m: 1.5, alignItems: 'center' }}>
+							<div>
+								<Typography
+									textAlign='center'
+									sx={{
+										m: 2,
+										display: { xs: 'none', md: 'flex' },
+										fontFamily: 'roboto',
+										fontWeight: 700,
+										color: 'inherit',
+										textDecoration: 'none',
+									}}
+								>
+									¿Qué te parecio el servicio brindado?
+								</Typography>
+							</div>
+							<div className={style.selectContainer}>
+								<InputLabel id='demo-multiple-chip-label'>
+									Seleciona un apartado
+								</InputLabel>
 
+								<Select
+									className={style.select}
+									required
+									labelId='demo-multiple-chip-label'
+									id='demo-multiple-chip'
+									name='title'
+									value={input.title}
+									onChange={handleChange}
+									input={<OutlinedInput label='Seleciona un apartado' />}
+									renderValue={(value) => (
+										<Box sx={{ display: 'flex', flexWrap: 'wrap', gap: 0.5 }}>
+											<Chip key={value} label={value} />
+										</Box>
+									)}
+									MenuProps={MenuProps}
+								>
+									{dataGym.map((name) => (
+										<MenuItem
+											key={name}
+											value={name}
+											style={getStyles(name, input.title, theme)}
+										>
+											{name}
+										</MenuItem>
+									))}
+								</Select>
+							</div>
 
-    };
+							{input.title === 'Staff' && (
+								<Select
+									className={style.selectStaff}
+									id='input-base'
+									name='staff'
+									// displayEmpty
+									value={input.staff}
+									onChange={handleChange}
+									renderValue={(selected) => {
+										if (selected.length === 0) {
+											return <em>Staff</em>;
+										}
+										return selected;
+									}}
+									MenuProps={MenuProps}
+									// inputProps={{ 'aria-label': 'Without label' }}
+								>
+									<MenuItem disabled value=''>
+										<em>Staff</em>
+									</MenuItem>
+									{staff?.map((value, i) => (
+										<MenuItem value={value} key={i}>
+											{value.name}
+										</MenuItem>
+									))}
+								</Select>
+							)}
 
-
-
-    const HandleSubmit = async (e) => {
-        e.preventDefault()
-        await addFeedback({
-            title: input.title,
-            staff: input.staff,
-            description: input.description,
-            score: input.score,
-            staffId: input.staffId
-        }).unwrap();
-        setInput({
-            title: [],
-            staff: "",
-            description: "",
-            score: 0,
-            staffId: 0,
-        })
-        alert("Gracias por dejarnos tu Feedback")
-        navigate("/home")
-    }
-
-    function getLabelText(value) {
-        return `${value} Star${value !== 1 ? 's' : ''}, ${labels[value]}`;
-    }
-
-    return (
-        <>
-
-            <NavBar />
-            <div className={style.mainContainer} >
-                <div className={style.mainContainerForm} >
-                    <form onSubmit={HandleSubmit}  >
-
-
-                        <FormControl sx={{ m: 1.5, width: 300 }} >
-                            <div>
-                                <img className={style.logoGym} src="https://cms-assets.tutsplus.com/cdn-cgi/image/width=850/uploads/users/2660/posts/32516/image/online-logo-generator-for-a-power-fitness-gym-2457j-93-el.jpg" />
-                            </div>
-                            <div>
-                                <Typography textAlign="center" sx={{
-                                    m: 3,
-                                    display: { xs: 'none', md: 'flex' },
-                                    fontFamily: 'roboto',
-                                    fontWeight: 700,
-                                    color: 'inherit',
-                                    textDecoration: 'none',
-                                }}>¿Qué te parecio el servicio brindado?</Typography>
-                            </div>
-
-                            <InputLabel sx={{ m: 0.3, width: 300, marginTop: 52.5 }} id="demo-multiple-chip-label"  >Seleciona un apartado </InputLabel>
-
-                            <Select className={style.select}
-                                required
-                                labelId="demo-multiple-chip-label"
-                                id="demo-multiple-chip"
-                                name="title"
-                                value={input.title}
-                                onChange={handleChange}
-                                input={<OutlinedInput label="Seleciona un apartado" />}
-                                renderValue={(value) => (
-                                    <Box sx={{ display: 'flex', flexWrap: 'wrap', gap: 0.5 }}>
-                                        <Chip key={value} label={value} />
-                                    </Box>
-                                )}
-                                MenuProps={MenuProps}
-                            >
-                                {dataGym.map((name) => (
-                                    <MenuItem key={name} value={name} style={getStyles(name, input.title, theme)}>
-                                        {name}
-                                    </MenuItem>
-                                ))}
-                            </Select>
-
-                            {(input.title === "Staff") &&
-                                <Select className={style.selectStaff}
-
-                                    id="input-base"
-                                    name="staff"
-                                    // displayEmpty
-                                    value={input.staff}
-                                    onChange={handleChange}
-                                    renderValue={(selected) => {
-                                        if (selected.length === 0) {
-                                            return <em>Staff</em>;
-                                        }
-                                        return selected
-                                    }}
-                                    MenuProps={MenuProps}
-                                // inputProps={{ 'aria-label': 'Without label' }}
-                                >
-                                    <MenuItem disabled value="">
-                                        <em>Staff</em>
-                                    </MenuItem>
-                                    {staff?.map((value, i) => (
-                                        <MenuItem value={value} key={i} >
-                                            {value.name}
-                                        </MenuItem>
-                                    )
-                                    )}
-                                </Select>
-                            }
-
-
-
-                            <Box required sx={{
-                                width: 300,
-                                display: 'flex',
-                                justifyContent: "center",
-                                alignItems: 'center',
-                                marginTop: 3
-                            }}>
-                                <Rating
-                                    required
-                                    size="large"
-                                    name="score"
-                                    value={input.score}
-                                    precision={0.5}
-                                    getLabelText={getLabelText}
-                                    onChange={handleChange}
-                                    emptyIcon={<StarIcon style={{ opacity: 0.55 }} fontSize="inherit" />} />
-                            </Box>
-                            {/* {errors.score && <p className={style.danger} >{errors.score}</p>} */}
-                            <div className={style.textField} >
-                                <Typography textAlign="center" sx={{
-                                    m: 3,
-                                    display: { xs: 'none', md: 'flex' },
-                                    fontFamily: 'roboto',
-                                    fontWeight: 700,
-                                    color: 'inherit',
-                                    textDecoration: 'none',
-                                }}>Dejanos tu comentario para poder mejorar</Typography>
-                                <TextField sx={{ m: 1, width: 300 }}
-                                    required
-                                    name="description"
-                                    variant="outlined"
-                                    placeholder="Comparti tu experiencia"
-                                    multiline
-                                    onChange={handleChange}
-                                    value={input.description}
-                                    rows={2}
-
-                                />
-                            </div>
-                            <div className={style.botonContainer} >
-                                <Button className={style.boton} type="submit" sx={{
-                                    paddingRight: '25px',
-                                    paddingLeft: '25px',
-                                    marginBottom: '10px',
-                                    color: 'white',
-                                    borderRadius: '6px',
-                                    background: '#2779ff',
-                                    alignItems: "center",
-                                    "&:hover": { backgroundColor: '#5151519c', transition: "1s" }
-                                }}> Submit </Button>
-                            </div>
-
-                        </FormControl>
-
-
-                    </form>
-                </div>
-            </div>
-        </>
-    )
-
-}
-
-
+							<Box
+								required
+								sx={{
+									width: 300,
+									display: 'flex',
+									justifyContent: 'center',
+									alignItems: 'center',
+									marginTop: 3,
+								}}
+							>
+								<Rating
+									required
+									size='large'
+									name='score'
+									value={input.score}
+									precision={0.5}
+									getLabelText={getLabelText}
+									onChange={handleChange}
+									emptyIcon={
+										<StarIcon style={{ opacity: 0.55 }} fontSize='inherit' />
+									}
+								/>
+							</Box>
+							{/* {errors.score && <p className={style.danger} >{errors.score}</p>} */}
+							<div className={style.textField}>
+								<Typography
+									textAlign='center'
+									sx={{
+										marginTop: '24px',
+										display: { xs: 'none', md: 'flex' },
+										fontFamily: 'roboto',
+										fontWeight: 700,
+										color: 'inherit',
+										textDecoration: 'none',
+									}}
+								>
+									Dejanos tu comentario para poder mejorar
+								</Typography>
+								<TextField
+									sx={{ m: 1, width: 300 }}
+									required
+									name='description'
+									variant='outlined'
+									placeholder='Comparti tu experiencia'
+									multiline
+									onChange={handleChange}
+									value={input.description}
+									rows={2}
+								/>
+							</div>
+							<div className={style.botonContainer}>
+								<Button
+									className={style.boton}
+									type='submit'
+									sx={{
+										paddingRight: '25px',
+										paddingLeft: '25px',
+										marginBottom: '10px',
+										color: 'white',
+										borderRadius: '6px',
+										background: '#2779ff',
+										alignItems: 'center',
+										'&:hover': {
+											backgroundColor: '#5151519c',
+											transition: '1s',
+										},
+									}}
+								>
+									Submit
+								</Button>
+							</div>
+						</FormControl>
+					</form>
+				</div>
+			</div>
+		</>
+	);
+};
 
 export default FeedBack;
-
-
--- src/components/Staff/Staff.jsx
+++ src/components/Staff/Staff.jsx
@@ -1,40 +1,42 @@
-import React, { useEffect } from "react";
-import { staticstaff } from "./Helpers.js";
-import { fetchGetAllStaff } from "../../redux/actions/defaultAction.js";
-import { useSelector, useDispatch } from "react-redux";
-import CardStaff from "../CardStaff/CardStaff.jsx";
-import Style from "./Staff.module.css";
-import NavBar from "../NavBar/NavBar.jsx";
-import Footer from "../Footer/Footer.jsx";
+import React, { useEffect } from 'react';
+import { staticstaff } from './Helpers.js';
+import { fetchGetAllStaff } from '../../redux/actions/defaultAction.js';
+import { useSelector, useDispatch } from 'react-redux';
+import CardStaff from '../CardStaff/CardStaff.jsx';
+import Style from './Staff.module.css';
+import NavBar from '../NavBar/NavBar.jsx';
+import Footer from '../Footer/Footer.jsx';
 
 const Staff = () => {
-  const dispatch = useDispatch();
-  const { staff } = useSelector((state) => state.staff);
-  useEffect(() => {
-    dispatch(fetchGetAllStaff());
-  }, [dispatch]);
-  return (
-    <>
-      <NavBar />
-      <div className={Style.mainwrapper}>
-        {/* {console.log(staff)} */}
-        <h1 className={Style.stafftittle}>Staff Técnico</h1>
-        <hr className={Style.divisionline}></hr>
-        <div className={Style.cardswrapper}>
-          {staticstaff?.map((staff) => (
-            <CardStaff
-              key={staff.hashPassword}
-              name={staff.name}
-              linkedin={staff.linkedin}
-              rating={staff.rating}
-              img={staff.picture}
-              mail={staff.email}
-            />
-          ))}
-        </div>
-      </div>
-      <Footer />
-    </>
-  );
+	const dispatch = useDispatch();
+	const { staff } = useSelector((state) => state.staff);
+	useEffect(() => {
+		dispatch(fetchGetAllStaff());
+	}, [dispatch]);
+	return (
+		<>
+			<NavBar />
+			<div className={Style.mainwrapper}>
+				{/* {console.log(staff)} */}
+				<div className={Style.titleContainer}>
+					<h1 className={Style.stafftittle}>Staff Técnico</h1>
+					<hr className={Style.divisionline}></hr>
+				</div>
+				<div className={Style.cardswrapper}>
+					{staticstaff?.map((staff) => (
+						<CardStaff
+							key={staff.hashPassword}
+							name={staff.name}
+							linkedin={staff.linkedin}
+							rating={staff.rating}
+							img={staff.picture}
+							mail={staff.email}
+						/>
+					))}
+				</div>
+			</div>
+			<Footer />
+		</>
+	);
 };
 export default Staff;
