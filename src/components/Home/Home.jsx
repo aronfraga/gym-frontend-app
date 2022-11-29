7425f92ec84ecd072e79e6a662a881eb37cd8fd5
JoseManriqueBaena <josed3h@gmail.com>
2022-11-29 02:20:50 -0500

    dash board and carousel

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,48 +1,50 @@
-import React, { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { useAuth0 } from "@auth0/auth0-react";
-import { tokenRequest } from "../../redux/actions/defaultAction";
-import { getToken } from "../../services/cookies";
-import NavBar from "../NavBar/NavBar";
-import Footer from "../Footer/Footer";
-import Loading from "../Loading/Loading";
-import Carrusel from "../Carousel/Carrusel";
-import Login from "../Login/Login";
-import Planes from "../Planes/Planes";
-import PrevRoutines from "../PrevRoutines/PrevRoutines";
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useAuth0 } from '@auth0/auth0-react';
+import { tokenRequest } from '../../redux/actions/defaultAction';
+import { getToken } from '../../services/cookies';
+import NavBar from '../NavBar/NavBar';
+import Footer from '../Footer/Footer';
+import Loading from '../Loading/Loading';
+import Carrusel from '../Carousel/Carrusel';
+import Login from '../Login/Login';
+import Planes from '../Planes/Planes';
+import PrevRoutines from '../PrevRoutines/PrevRoutines';
+import { PrevShop } from '../PrevShop/PrevShop';
 
 const Home = () => {
-  const dispatch = useDispatch();
-  const { user, isAuthenticated, isLoading, logout } = useAuth0();
-  const { tokenIsValid } = useSelector((state) => state.tokenIsValid);
+	const dispatch = useDispatch();
+	const { user, isAuthenticated, isLoading, logout } = useAuth0();
+	const { tokenIsValid } = useSelector((state) => state.tokenIsValid);
 
-  useEffect(() => {
-    if (isAuthenticated) {
-      if (!getToken() && tokenIsValid) {
-        dispatch(
-          tokenRequest({
-            name: user.given_name || user.nickname,
-            email: user.email,
-            password: user.sub,
-          })
-        );
-      }
-      if (!tokenIsValid) logout();
-    }
-  }, [dispatch, isAuthenticated, tokenIsValid]);
+	useEffect(() => {
+		if (isAuthenticated) {
+			if (!getToken() && tokenIsValid) {
+				dispatch(
+					tokenRequest({
+						name: user.given_name || user.nickname,
+						email: user.email,
+						password: user.sub,
+					})
+				);
+			}
+			if (!tokenIsValid) logout();
+		}
+	}, [dispatch, isAuthenticated, tokenIsValid]);
 
-  if (isLoading) return <Loading />;
-  if (!isAuthenticated) return <Login />;
+	if (isLoading) return <Loading />;
+	if (!isAuthenticated) return <Login />;
 
-  return (
-    <div>
-      <NavBar />
-      <Carrusel />
-      <PrevRoutines />
-      <Planes />
-      <Footer />
-    </div>
-  );
+	return (
+		<div>
+			<NavBar />
+			<Carrusel />
+			<PrevRoutines />
+			<PrevShop />
+			<Planes />
+			<Footer />
+		</div>
+	);
 };
 
 export default Home;
