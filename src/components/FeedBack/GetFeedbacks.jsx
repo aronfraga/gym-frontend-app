c775b3ed14dcbd389aeacf7adce68057db7700e3
JoseManriqueBaena <josed3h@gmail.com>
2022-12-01 01:26:01 -0500

    Fixed dashboard and profile

--- src/components/FeedBack/GetFeedbacks.jsx
+++ src/components/FeedBack/GetFeedbacks.jsx
@@ -1,51 +1,65 @@
-import React from "react";
-import { useGetAllFeedbacksQuery } from "../../redux/query/api";
-import { Card, CardActionArea } from "@mui/material";
-import style from './GetFeedbacks.module.css'
-import NavBar from "../NavBar/NavBar";
-
+import React from 'react';
+import { useGetAllFeedbacksQuery } from '../../redux/query/api';
+import { Card, CardActionArea } from '@mui/material';
+import style from './GetFeedbacks.module.css';
+import NavBar from '../NavBar/NavBar';
+import IconButton from '@mui/material/IconButton';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import { useNavigate } from 'react-router-dom';
 
 export default function GetFeedbacks() {
+	const { data: feedbacks } = useGetAllFeedbacksQuery();
+	const navigate = useNavigate();
 
-    const { data: feedbacks } = useGetAllFeedbacksQuery();
+	function handlerClickBack() {
+		navigate(-1);
+	}
 
-    return (
-        <div>
-            <NavBar />
-            <div className={style.mainContainer}>
-                <h2 className={style.tittle}>Feedbacks</h2>
-                <hr className={style.divisionline}></hr>
-                <div className={style.cardsclasses} >
-                    {feedbacks?.products.map((value, i) => (
-                        <Card key={i}
-                            className={style.cardPlan}
-                            sx={{
-                                display: 'flex',
-                                justifyContent: 'center',
-                                width: '100%',
-                                maxWidth: '400px',
-                                height: 'fit-content',
-                                border: '1px solid var(--tertiary-color) ',
-                                borderRadius: '10px',
-                                backgroundColor: 'white',
-                                transition: 'all 0.2s ease-out',
-                            }}
-                        >
-
-                            <div className={style.containerBenefits}>
-                                <h2>Comentario: {value.title}</h2>
-                                <h2>Staff: {value.staff}</h2>
-                                <h2>Score: {value.score}</h2>
-                                <h2>Description: {value.description}</h2>
-
-                            </div>
-
-
-                        </Card>
-                    ))}
-                </div>
-            </div>
-        </div>
-
-    )
+	return (
+		<div>
+			<NavBar />
+			<div className={style.mainContainer}>
+				<div className={style.goBack}>
+					<div>
+						<IconButton
+							onClick={handlerClickBack}
+							sx={{
+								color: 'var(--secondary-color)',
+							}}
+						>
+							<ArrowBackIcon sx={{ fontSize: 30 }} />
+						</IconButton>
+					</div>
+					<h2 className={style.tittle}>Feedbacks</h2>
+				</div>
+				<hr className={style.divisionline}></hr>
+				<div className={style.cardsclasses}>
+					{feedbacks?.products.map((value, i) => (
+						<Card
+							key={i}
+							className={style.cardPlan}
+							sx={{
+								display: 'flex',
+								justifyContent: 'center',
+								width: '100%',
+								maxWidth: '400px',
+								height: 'fit-content',
+								border: '1px solid var(--tertiary-color) ',
+								borderRadius: '10px',
+								backgroundColor: 'white',
+								transition: 'all 0.2s ease-out',
+							}}
+						>
+							<div className={style.containerBenefits}>
+								<h2>Comentario: {value.title}</h2>
+								<h2>Staff: {value.staff}</h2>
+								<h2>Score: {value.score}</h2>
+								<h2>Description: {value.description}</h2>
+							</div>
+						</Card>
+					))}
+				</div>
+			</div>
+		</div>
+	);
 }
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -2,7 +2,10 @@ import React, { useEffect, useState } from 'react';
 import NavBar from '../NavBar/NavBar';
 import { Link, useNavigate } from 'react-router-dom';
 import { useAuth0 } from '@auth0/auth0-react';
-import { useGetAllUsersQuery, useSetNewImgMutation } from '../../redux/query/api';
+import {
+	useGetAllUsersQuery,
+	useSetNewImgMutation,
+} from '../../redux/query/api';
 import { useGetUserProfileQuery } from '../../redux/query/ApiEcommerce';
 import Loading from '../Loading/Loading';
 import Style from './Profile.module.css';
@@ -11,63 +14,56 @@ import { PhotoCamera } from '@mui/icons-material';
 import { Button } from '@mui/material';
 import { uploadUserImage } from './Helpers';
 import { useDispatch, useSelector } from 'react-redux';
-import { setNewPlan } from "../../redux/actions/defaultAction";
+import { setNewPlan } from '../../redux/actions/defaultAction';
 
 const Profile = () => {
 	const navigate = useNavigate();
 	const dispatch = useDispatch();
 	const [setImg] = useSetNewImgMutation();
-	const { user } = useAuth0();
-	const email = user?.email;
 	const [isShown, setIsShown] = useState(false);
 	const [input, setInput] = useState({ imgUrl: '' });
-	const { data, isSuccess, isLoading } = useGetAllUsersQuery();
-	const { data: profile, isLoading: loading } = useGetUserProfileQuery();
-	const { alertDelivery } = useSelector((state) => state.alertDelivery);
+	const { data: profile, isLoading, isSuccess } = useGetUserProfileQuery();
+	const rol = profile?.role;
 	const urlChanged = window.location.search;
 	const urlParams = new URLSearchParams(urlChanged);
 	const fecha = new Date(profile?.membresyExpDate);
-  
+
 	const purchaseStatus = {
-    payed: urlParams.get("status") === "approved" ? true : false,
-    paymentMethod: urlParams.get("payment_type"),
-    purchaseId: urlParams.get("preference_id"),
-  };
-
-	useEffect(() => {
-    if(purchaseStatus.payed) {
-			dispatch(setNewPlan(purchaseStatus))
-			navigate("/home")
-		}
-  },[]);
-
-	const mydata = () => {
-		if (isSuccess) {
-			let actual_user = data?.find((e) => e.email === email);
-			return actual_user;
-		}
+		payed: urlParams.get('status') === 'approved' ? true : false,
+		paymentMethod: urlParams.get('payment_type'),
+		purchaseId: urlParams.get('preference_id'),
 	};
 
-	const datos = mydata();
-	let rol = datos?.role;
+	useEffect(() => {
+		if (purchaseStatus.payed) {
+			dispatch(setNewPlan(purchaseStatus));
+			navigate('/home');
+		}
+	}, []);
 
 	function renderPlan() {
-		if(loading) return <p>Loading...</p>
-		return <p>Tu plan vence el {fecha.toLocaleDateString()}</p> 
+		if (isLoading) return <p>Loading...</p>;
+		return <p>Tu plan vence el {fecha.toLocaleDateString()}</p>;
+	}
+
+	function renderPlanExpired() {
+		if (isLoading) return <p>Loading...</p>;
+		return (
+			<p>
+				Tu plan se encuentra vencido, puedes renovarlo ahora mismo haciendo clic
+				aquí abajo
+			</p>
+		);
 	}
 
 	const funcionalidad = () => {
-		let date1 = new Date(datos?.membresyExpDate);
-		let date2 = Date.now();
-		let diff = Math.floor((date1 - date2) / (1000 * 60 * 60 * 24));
 		if (isSuccess && rol !== 'Admin') {
-			if (diff < 0) {
+			if (profile?.expiredMembresy) {
 				return (
 					<div>
-						{renderPlan()}
+						{renderPlanExpired()}
 						<Link to='/planes' style={{ textDecoration: 'none' }}>
 							<Button
-								onClick={handlerSaveButton}
 								variant='contained'
 								sx={{
 									width: '100%',
@@ -89,7 +85,6 @@ const Profile = () => {
 						{renderPlan()}
 						<Link to='/planes' style={{ textDecoration: 'none' }}>
 							<Button
-								onClick={handlerSaveButton}
 								variant='contained'
 								sx={{
 									width: '100%',
@@ -109,25 +104,22 @@ const Profile = () => {
 		} else if (isSuccess && rol === 'Admin') {
 			return (
 				<div>
-					<p>Eres administrador, puedes acceder</p>
-					<p> a la siguiente funcionalidad</p>
-					<Link to={'/admdashboard'} 
-								style={{ textDecoration: 'none' }}>
-                        <Button
-                            onClick={handlerSaveButton}
-                            variant='contained'
-                            sx={{
-                                width: '100%',
-                                background: '#0d0d6b',
-                                '&:hover': {
-                                    backgroundColor: '#62629f',
-                                    transition: '0.4s',
-                                },
-                            }}
-                        >
-                            Dashboard
-                        </Button>
-                    </Link>
+					<p>Eres administrador, puedes acceder a la siguiente funcionalidad</p>
+					<Link to={'/admdashboard'} style={{ textDecoration: 'none' }}>
+						<Button
+							variant='contained'
+							sx={{
+								width: '100%',
+								background: '#0d0d6b',
+								'&:hover': {
+									backgroundColor: '#62629f',
+									transition: '0.4s',
+								},
+							}}
+						>
+							Dashboard
+						</Button>
+					</Link>
 				</div>
 			);
 		}
@@ -157,7 +149,7 @@ const Profile = () => {
 					<div className={Style.ProfileWrapper}>
 						<div className={Style.PhotoandEdit}>
 							<div className={Style.PhotoContainer}>
-								<img className={Style.img} src={datos?.imgUrl} />{' '}
+								<img className={Style.img} src={profile?.imgUrl} />{' '}
 							</div>
 							<div className={Style.buttonContainer}>
 								<Button
@@ -215,8 +207,8 @@ const Profile = () => {
 							)}
 						</div>
 						<div className={Style.InfoProfile}>
-							<h3>Nombre: {datos?.name}</h3>
-							<h3>E-mail: {datos?.email}</h3>
+							<h3>Nombre: {profile?.name}</h3>
+							<h3>E-mail: {profile?.email}</h3>
 							{funcionalidad()}
 						</div>
 					</div>
