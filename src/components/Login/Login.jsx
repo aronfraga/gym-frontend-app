35686ef789f108de41069c3b8aecec45a359c931
JoseManriqueBaena <josed3h@gmail.com>
2022-12-07 12:39:15 -0500

    ¿update landing

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,38 +1,47 @@
 import React from 'react';
 import { Button } from '@mui/material';
 import { useAuth0 } from '@auth0/auth0-react';
-import './Login.css';
+import style from './Login.module.css';
+import Typewriter from 'typewriter-effect';
 
 const Landing = () => {
 	const { loginWithRedirect } = useAuth0();
 	return (
-		// <div className="loginContainer">
-		//   <h1 className="loginText">Unete a la comunidad del App Gym</h1>
-		//   <Button variant="contained" onClick={() => loginWithRedirect()}>
-		//     Iniciar Sesion
-		//   </Button>
-		// </div>
-		<div className='loginContainer'>
-			<div className='card transition'>
-				<h2 className='transition'>Gym Fit</h2>
-				<p className='parrafo'>Unete a nuestra comunidad.</p>
-				<div className='cta-container transition'>
-					<Button
-						className='cta'
-						variant='contained'
-						sx={{
-							background: 'var(--primary-color)',
-							'&:hover': {
-								backgroundColor: 'var(--secondary-color)',
-								transition: '0.4s',
-							},
-						}}
-						onClick={() => loginWithRedirect()}
-					>
-						Iniciar Sesion
-					</Button>
+		<div className={style.mainContainer}>
+			<div className={style.infoContainer}>
+				<div className={style.titlesContainer}>
+					<h1 className={style.title1}>
+						Bienvenido a nuestra comunidad Gymfit.
+					</h1>
+					<div className={style.divTypewriter}>
+						<Typewriter
+							onInit={(typewriter) => {
+								typewriter
+									.typeString('Suscríbete a nuestro gym desde 2500 por mes')
+									.pauseFor(3000)
+									.deleteAll()
+									.typeString('Inicia sesión para entrar en acción!')
+									.start();
+							}}
+						/>
+					</div>
 				</div>
-				<div className='card_circle transition'></div>
+				<Button
+					className='cta'
+					variant='contained'
+					sx={{
+						width: '300px',
+						height: '50px',
+						background: 'var(--primary-color)',
+						'&:hover': {
+							backgroundColor: 'var(--secondary-color)',
+							transition: '0.4s',
+						},
+					}}
+					onClick={() => loginWithRedirect()}
+				>
+					Iniciar Sesion
+				</Button>
 			</div>
 		</div>
 	);
